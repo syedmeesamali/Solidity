768a5782e8cdc03350f9803dd6104e64442b4d20
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-18 16:13:34 +0400

    Structs and Arrays in Solidity

---
+++ 1_Review/carContract.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.5.1;
+
+//This is the main contract where whole working and functioning will exist
+contract CarContract
+{
+    struct CAR 
+    {
+        string name;
+        int year;
+        
+    }
+
+}
