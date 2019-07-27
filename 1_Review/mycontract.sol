bda19f7aff31ea0f5c1fc225c8063421818f357f
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-27 17:38:57 +0400

    Full simple solidity contract

--- 1_Review/mycontract.sol
+++ 1_Review/mycontract.sol
@@ -1,7 +1,24 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.1;
 
 //Review of solidity contracts
 contract MyContract
 {
     string value;       //available across the contract
+    
+    constructor() public 
+    {
+        value = "Some Value";
+    }
+
+    //Function to get some string value
+    function get() public view returns(string)
+    {
+        return value;
+    }
+
+    //Function to set some string value
+    function set(string _value) public 
+    {
+        value = _value;
+    }
 }
