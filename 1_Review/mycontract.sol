986714df8cfc8a233d3528a049b3217a2bf2859d
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-27 17:52:30 +0400

    Updates as per latest version

--- 1_Review/mycontract.sol
+++ 1_Review/mycontract.sol
@@ -3,21 +3,15 @@ pragma solidity ^0.5.1;
 //Review of solidity contracts
 contract MyContract
 {
-    string value;       //available across the contract
+    string public value;       //available across the contract
     
     constructor() public 
     {
         value = "Some Value";
     }
 
-    //Function to get some string value
-    function get() public view returns(string)
-    {
-        return value;
-    }
-
     //Function to set some string value
-    function set(string _value) public 
+    function set(string memory _value) public 
     {
         value = _value;
     }
