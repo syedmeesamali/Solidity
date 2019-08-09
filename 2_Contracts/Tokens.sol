20cf1de43b7d5258f0f739d43bd1f970f083fcec
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-10 00:04:22 +0400

    Purchase function tracking

--- 2_Contracts/Tokens.sol
+++ 2_Contracts/Tokens.sol
@@ -6,15 +6,21 @@ contract MyContract {
     mapping(address => uint256) public balances;
     address payable wallet;
     
-    //As per latest solidity update wallets should have keyword PAYABLE
+    event Purchase( address indexed _buyer, uint256 _amount );
+    
     constructor (address payable _wallet) public {
         wallet = _wallet;
+    }
+    
+    function() external payable {
+        buyToken();
     }
     
     function buyToken() public payable{
         //buy a token 
         balances[msg.sender] += 1;
         wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1);
     }
     
 }
