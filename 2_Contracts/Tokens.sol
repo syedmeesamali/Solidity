0fc6f1ccc31b18facdecb7925ec819390c94b50c
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-09 22:48:25 +0400

    wallet should have payable

--- 2_Contracts/Tokens.sol
+++ 2_Contracts/Tokens.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.5.1;
 contract MyContract {
     
     mapping(address => uint256) public balances;
-    address wallet;
+    address payable wallet;
     
-    function buyToken() {
+    //As per latest solidity update wallets should have keyword PAYABLE
+    constructor (address payable _wallet) public {
+        wallet = _wallet;
+    }
+    
+    function buyToken() public payable{
         //buy a token 
         balances[msg.sender] += 1;
         wallet.transfer(msg.value);
